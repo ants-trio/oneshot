fc0deafb5029fe2f7f6127439d3601973056c483
Seungmin Lim <seungmin9066@gmail.com>
2022-03-18 02:21:20 +0900

    [리팩터링] 로그인 서비스와 검증기 역할에 맞게 메서드 재배치

--- land/src/main/java/happyhouse_team02/land/landservice/service/LoginService.java
+++ land/src/main/java/happyhouse_team02/land/landservice/service/LoginService.java
@@ -1,10 +1,12 @@
 package happyhouse_team02.land.landservice.service;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface LoginService {
 
 	/**
-	 * @return 빈 문자열이면 로그인 실패
+	 * 로그인
+	 * 세션 키와 email 을 매핑하여 저장한다.
 	 */
-	String login(String email, String password);
-
+	void login(HttpServletRequest request, String email);
 }
