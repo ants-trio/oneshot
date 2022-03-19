e972b417046f4f3bae1f654d6e9f48beb08f7a21
Seungmin Lim <seungmin9066@gmail.com>
2022-03-19 17:32:47 +0900

    [리팩터링] 폴더 구조 개선 및 Validator 리팩터링

---
+++ land/src/main/java/happyhouse_team02/land/landservice/web/ValidatorAdapter.java
@@ -0,0 +1,8 @@
+package happyhouse_team02.land.landservice.web;
+
+public interface ValidatorAdapter {
+
+	boolean supports (Object object);
+
+	void validate();
+}
---
+++ land/src/main/java/happyhouse_team02/land/landservice/web/adapter/LoginValidatorAdapter.java
@@ -0,0 +1,15 @@
+package happyhouse_team02.land.landservice.web.adapter;
+
+import happyhouse_team02.land.landservice.web.ValidatorAdapter;
+
+public class LoginValidatorAdapter implements ValidatorAdapter {
+	@Override
+	public boolean supports(Object object) {
+		return false;
+	}
+
+	@Override
+	public void validate() {
+
+	}
+}
