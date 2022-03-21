c2a27bddc048d3c9701f4c313f34b552230ad3aa
Seungmin Lim <seungmin9066@gmail.com>
2022-03-21 19:16:43 +0900

    [수정] 상수를 인터페이스로 관리

--- land/src/main/java/happyhouse_team02/land/landservice/exception/ExceptionMessage.java
+++ land/src/main/java/happyhouse_team02/land/landservice/exception/ExceptionMessage.java
@@ -1,18 +1,24 @@
 package happyhouse_team02.land.landservice.exception;
 
-public enum ExceptionMessage {
-	DUPLICATED_MEMBER_MASSAGE("[ERROR] 이미 존재하는 회원입니다."),
-	NO_SUCH_MEMBER_MASSAGE("[ERROR] 존재하지 않는 회원입니다."),
-	NO_SUCH_POST_MASSAGE("[ERROR] 존재하지 않는 포스트입니다."),
-	PASSWORD_DOES_NOT_MATCH_MASSAGE("[ERROR] 비밀번호가 일치하지 않습니다.");
-	private final String message;
+public interface ExceptionMessage {
+	/**
+	 * 조회한 회원이 이미 가입되어 있는 경우
+	 */
+	String DUPLICATED_MEMBER_MASSAGE = "[ERROR] 이미 존재하는 회원입니다.";
 
-	ExceptionMessage(final String message) {
-		this.message = message;
-	}
+	/**
+	 * 조회한 회원을 찾을 수 없는 경우
+	 */
+	String 	NO_SUCH_MEMBER_MASSAGE = "[ERROR] 존재하지 않는 회원입니다.";
 
-	@Override
-	public String toString() {
-		return message;
-	}
+	/**
+	 * 조회한 게시물을 찾을 수 없는 경우
+	 */
+	String  NO_SUCH_POST_MASSAGE = "[ERROR] 존재하지 않는 포스트입니다.";
+
+	/**
+	 * 비밀번호가 일치하지 않는 경우
+	 */
+	String PASSWORD_DOES_NOT_MATCH_MASSAGE = "[ERROR] 비밀번호가 일치하지 않습니다.";
+
 }
