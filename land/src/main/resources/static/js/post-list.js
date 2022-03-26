b60536bb186ce37d0dc3aa0287ef3ac27a70ad10
yyeopp <letsksy@naver.com>
2022-03-26 22:15:48 +0900

    [추가] JS 파일 기능별로 추가

---
+++ land/src/main/resources/static/js/post-list.js
@@ -0,0 +1,23 @@
+/*
+
+initiateBoard();
+expressBoard();
+
+
+
+*/
+
+$(function () {
+  // 열자마자 ajax로 찍어서 response 가져왔다고 가정
+  let responseTest = {
+    postId: 333,
+    createDate: "2022-03-26 22:04:03",
+    lastUpdateDate: "2022-03-26 22:04:03",
+    postContent: "내용입니다",
+    postLike: false,
+    postTitle: "제목입니다",
+    memberName: "임승민",
+  };
+
+  console.log(responseTest);
+});
