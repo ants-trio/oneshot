7858160ea9464283240e1a80035dc323b45f5885
Seungmin Lim <seungmin9066@gmail.com>
2022-03-29 13:26:42 +0900

    [수정] 컴포넌트 추가

--- land/src/main/java/happyhouse_team02/land/landservice/service/comment/CommentServiceImpl.java
+++ land/src/main/java/happyhouse_team02/land/landservice/service/comment/CommentServiceImpl.java
@@ -3,5 +3,5 @@ package happyhouse_team02.land.landservice.service.comment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CommentServiceImpl {
+public class CommentServiceImpl implements CommentService{
 }
