ed278ce3a7df06f97b58d83214e83c021e8b65ed
Seungmin Lim <seungmin9066@gmail.com>
2022-04-03 03:05:07 +0900

    [수정] OSIV를 false로 설정, 모든 트랜잭션은 서비스안에서 일어난다.

--- land/src/main/java/happyhouse_team02/land/landservice/repository/post/PostRepository.java
+++ land/src/main/java/happyhouse_team02/land/landservice/repository/post/PostRepository.java
@@ -2,12 +2,17 @@ package happyhouse_team02.land.landservice.repository.post;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import happyhouse_team02.land.landservice.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+	@EntityGraph(attributePaths = {"member"})
+	Page<Post> findAll(Pageable pageable);
 
 	@EntityGraph(attributePaths = {"member", "comments"})
 	Optional<Post> findDistinctById(Long id);
