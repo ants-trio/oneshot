7dcec9d3ad618837c87cc474892adeb48fa391e9
Seungmin Lim <seungmin9066@gmail.com>
2022-04-04 15:06:06 +0900

    [리팩터링] Post HTTP API pageable로 입력을 받게 대체

--- land/src/main/java/happyhouse_team02/land/landservice/api/post/PostApiController.java
+++ land/src/main/java/happyhouse_team02/land/landservice/api/post/PostApiController.java
@@ -1,8 +1,12 @@
 package happyhouse_team02.land.landservice.api.post;
 
+import static org.springframework.data.domain.Sort.Direction.*;
+
 import javax.validation.constraints.NotEmpty;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import happyhouse_team02.land.landservice.api.SuccessResponseResult;
@@ -34,9 +37,8 @@ public class PostApiController {
 
 
 	@GetMapping
-	public SuccessResponseResult getPosts(@RequestParam int pageNo,
-										  @RequestParam int amount) {
-		Page<PostPageResponseDto> postsSummary = postService.findPostPages(pageNo, amount).map(PostPageResponseDto::new);
+	public SuccessResponseResult getPosts(@PageableDefault(sort = "createdDate", direction = DESC) Pageable pageable) {
+		Page<PostPageResponseDto> postsSummary = postService.findPostPages(pageable).map(PostPageResponseDto::new);
 
 		return new SuccessResponseResult(new GetPostsResponse(postsSummary));
 	}
